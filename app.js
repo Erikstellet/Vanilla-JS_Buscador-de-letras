7c93f44e0ad6891d864dda3199b40b5f251773ff
Erikstellet <erikstellet@gmail.com>
2021-03-13 01:33:10 -0300

    Initializing Project

---
+++ app.js
@@ -0,0 +1,47 @@
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#search');
+const songsContainer = document.querySelector('#songs-container');
+const prevAndNextContainer = document.querySelector('#prev-and-next-container');
+
+const apiURL = `https://api.lyrics.ovh`;
+
+const insertsongsIntoPage = songsInfo => 
+{
+    songsContainer.innerHTML = songsInfo.data.map(song =>
+    {
+       return `<li class="song">
+                    <span class="song-artist">
+                        <strong>${song.artist.name}</strong>
+                         - ${song.title}
+                    </span>
+
+                    <button class="btn" data-artist="${song.artist.name}"
+                            data-song-title="${song.title}"> Ver Letra
+                    </button>
+               </li>`
+    }).join('')
+};
+
+const fetchSongs = async term =>
+{
+    const response = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await response.json();
+    
+    insertsongsIntoPage(data);
+}
+
+form.addEventListener('submit', event => 
+{
+    event.preventDefault()
+    const searchTerm = searchInput.value.trim();
+    
+    if(!searchTerm)
+    {
+        songsContainer.innerHTML = `<li class="warning-message">
+                                        Por favor, digite um termo válido      
+                                    </li>`
+        return 
+    }
+
+    fetchSongs(searchTerm)
+})
